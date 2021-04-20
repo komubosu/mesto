2503107afe6d3ed4da2bccb614dc6afb3ff29d08
Michael Dokuchaev <redisooon@ya.ru>
2021-04-20 23:13:08 +0700

    hotfix v1.2

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 const openPopupButton = document.querySelector('.profile__edit-btn');
 const closePopupButton = document.querySelector('.popup__close-btn');
-const saveFormButton = document.querySelector('.form__submit-btn')
 const popup = document.querySelector('.popup');
 
 let profileName = document.querySelector('.profile__title');
 let profileSub = document.querySelector('.profile__subtitle');
 
-let inputProfileName = document.querySelector('.input__profilename');
-let inputProfileSub = document.querySelector('.input__profilesub');
+let profileEdit = document.profileEdit;
+let inputProfileName = profileEdit.inputProfileName;
+let inputProfileSub = profileEdit.inputProfileSub;
 
 function openPopup () {
   inputProfileName.value = profileName.textContent;
@@ -20,7 +20,9 @@ function closePopup () {
   popup.classList.remove('popup_opened');
 };
 
-function saveForm () {
+function saveForm (e) {
+  e.preventDefault();
+
   profileName.textContent = inputProfileName.value;
   profileSub.textContent = inputProfileSub.value;
 
@@ -31,4 +33,4 @@ openPopupButton.addEventListener('click', openPopup);
 
 closePopupButton.addEventListener('click', closePopup);
 
-saveFormButton.addEventListener('click', saveForm);
+profileEdit.addEventListener('submit', saveForm, true);
